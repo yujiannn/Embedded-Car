50402d5c873dff99fbf04ec1c0c878b85c3c99ab
Tan Pei Wen <peiwentan00@gmail.com>
2023-11-06 10:20:05 +0800

    updated code for wifi

--- drivers/wifi/wifi.c
+++ drivers/wifi/wifi.c
@@ -1,128 +1,62 @@
-#include "pico/cyw43_arch.h"
-#include <stdio.h>
-#include "pico/stdlib.h"
-//#include "hardware/gpio.h"
-//#include "hardware/timer.h"
-//#include "lwip/ip4_addr.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "ping.h"
-#include "message_buffer.h"
-
-#define TRIG_PIN 0   // Ultrasonic sensor TRIG pin
-#define ECHO_PIN 1   // Ultrasonic sensor ECHO pin
-
-// #define WIFI_SSID "YourWiFiSSID"      // Replace with your Wi-Fi SSID
-// #define WIFI_PASSWORD "YourPassword"  // Replace with your Wi-Fi password
-
-#define TIMEOUT 26100
-#define mbaTASK_MESSAGE_BUFFER_SIZE (60)
+#include "pico/cyw43_arch.h"  // Include the Pico Wi-Fi module library
+#include <stdio.h>            // Include standard input/output library
+#include "pico/stdlib.h"      // Include Pico standard library
+#include "hardware/gpio.h"    // Include hardware GPIO library
+#include "hardware/timer.h"   // Include hardware timer library
+#include "lwip/ip4_addr.h"    // Include LWIP IPv4 address library
+#include "FreeRTOS.h"         // Include FreeRTOS library
+#include "task.h"             // Include task management library
+#include "ping.h"             // Include ping library for network testing
+#include "message_buffer.h"   // Include message buffer library
 
 #ifndef PING_ADDR
-#define PING_ADDR "142.251.35.196"
+#define PING_ADDR "142.251.35.196"  // Define a default IP address to ping
 #endif
 
-#define TEST_TASK_PRIORITY (tskIDLE_PRIORITY + 1UL)
+#define TEST_TASK_PRIORITY (tskIDLE_PRIORITY + 1UL)  // Define the priority of the test task
 
-static MessageBufferHandle_t xControlMessageBuffer;
+static MessageBufferHandle_t xControlMessageBuffer;  // Declare a message buffer handle
 
-// void setupUltrasonicPins(uint trigPin, uint echoPin) {
-//     gpio_init(trigPin);
-//     gpio_init(echoPin);
-//     gpio_set_dir(trigPin, GPIO_OUT);
-//     gpio_set_dir(echoPin, GPIO_IN);
-// }
-
-// volatile uint64_t pulse_start_time = 0;
-// volatile uint64_t pulse_end_time = 0;
-
-// void echo_pin_isr(uint gpio, uint32_t events) {
-//     if (gpio_get(gpio)) {
-//         // Rising edge
-//         pulse_start_time = time_us_64();
-//     } else {
-//         // Falling edge
-//         pulse_end_time = time_us_64();
-//     }
-// }
-
-// uint64_t getPulse(uint trigPin) {
-//     pulse_start_time = 0;
-//     pulse_end_time = 0;
-
-//     gpio_put(trigPin, 1);
-//     sleep_us(10);
-//     gpio_put(trigPin, 0);
-
-//     // Wait for falling edge interrupt
-//     while (pulse_end_time == 0) {
-//         tight_loop_contents();
-//     }
-
-//     // Calculate pulse duration
-//     uint64_t pulse_duration = pulse_end_time - pulse_start_time;
-
-//     return pulse_duration;
-// }
-
-// uint64_t getCm(uint trigPin, uint echoPin) {
-//     uint64_t pulseLength = getPulse(trigPin);
-//     return pulseLength * 0.0343 / 2;  // Speed of sound in air is approximately 343 m/s
-// }
-
-// void ultrasonic_task(void *params) {
-//     uint trigPin = 0; // Replace with your TRIG pin number
-//     uint echoPin = 1; // Replace with your ECHO pin number
-
-//     setupUltrasonicPins(trigPin, echoPin);
-
-//     // Configure the ECHO pin for interrupts on both rising and falling edges
-//     gpio_set_irq_enabled_with_callback(echoPin, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, echo_pin_isr);
-
-//     printf("Ultrasonic Sensor Readings\n");
-
-//     while (1) {
-//         uint64_t distance_cm = getCm(trigPin, echoPin);
-//         printf("Distance: %llu cm\n", distance_cm);
-//         vTaskDelay(pdMS_TO_TICKS(1000));  // Delay between measurements
-//     }
-// }
-
+// Define a task to handle Wi-Fi operations
 void wifi_task(void *params) {
+    // Initialize the Pico Wi-Fi module
     if (cyw43_arch_init()) {
         printf("failed to initialize\n");
-        vTaskDelete(NULL);
+        vTaskDelete(NULL);  // Delete the task if initialization fails
     }
+
+    // Enable the Wi-Fi station mode
     cyw43_arch_enable_sta_mode();
+
     printf("Connecting to Wi-Fi...\n");
+
+    // Attempt to connect to the Wi-Fi network with a timeout of 30 seconds
     if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
         printf("failed to connect.\n");
-        vTaskDelete(NULL);
+        vTaskDelete(NULL);  // Delete the task if the connection fails
     } else {
         printf("Connected.\n");
     }
 
     ip_addr_t ping_addr;
-    ipaddr_aton(PING_ADDR, &ping_addr);
-    ping_init(&ping_addr);
+    ipaddr_aton(PING_ADDR, &ping_addr);  // Convert the PING_ADDR to an IP address
+    ping_init(&ping_addr);  // Initialize the ping functionality
 
     while (1) {
-        // not much to do as LED is in another task, and we're using RAW (callback) lwIP API
-        vTaskDelay(pdMS_TO_TICKS(100));
+        vTaskDelay(pdMS_TO_TICKS(100));  // Delay the task for 100 milliseconds
     }
 
-    cyw43_arch_deinit();
+    cyw43_arch_deinit();  // Deinitialize the Pico Wi-Fi module when the task exits
 }
 
 int main() {
-    stdio_init_all();
+    stdio_init_all();  // Initialize standard I/O
 
-    xControlMessageBuffer = xMessageBufferCreate(mbaTASK_MESSAGE_BUFFER_SIZE);
+    xControlMessageBuffer = xMessageBufferCreate(60);  // Create a message buffer
 
+    // Create and start the Wi-Fi task
     xTaskCreate(wifi_task, "WifiTask", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, NULL);
-    //xTaskCreate(ultrasonic_task, "UltrasonicTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    vTaskStartScheduler();  // Start the FreeRTOS task scheduler
 
-    vTaskStartScheduler();
-
-    return 0;
+    return 0;  // The main function typically does not return in embedded systems
 }
